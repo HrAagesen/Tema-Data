de1865f213fedf14c512f030e4239ee4d4b930c8
HrAagesen <33120123+HrAagesen@users.noreply.github.com>
2018-04-10 10:40:27 +0200

    Add files via upload

--- netshop/js/config.js
+++ netshop/js/config.js
@@ -73,7 +73,7 @@ $(function() {
 	simpleStore.init({
 
 		// brand can be text or image URL
-		brand : "SimpleStore",
+		brand : "http://mikkelaagesen.com/Webshop/images/eye_logo_banner.png",
 
 		// numder of products per row (accepts 1, 2 or 3)
 		numColumns : 3,
